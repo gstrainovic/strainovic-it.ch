74ec2e8991440bde1d848f8748b54c94fc537800
gstrainovic <g.strainovic@gmail.com>
2019-07-22 10:18:13 +0200

    test

--- layouts/default.vue
+++ layouts/default.vue
@@ -200,6 +200,7 @@
                               class="form-control"
                               type="email"
                               placeholder=" "
+                              value="contact"
                             />
                             <label
                               class="form-control-placeholder"
