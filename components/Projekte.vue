965243dd4fca23ec7087f73d45d9b6c35b0f1cc3
gstrainovic <g.strainovic@gmail.com>
2019-09-08 01:42:58 +0200

    trans

--- components/Projekte.vue
+++ components/Projekte.vue
@@ -4,41 +4,49 @@
       <div class="container">
         <div class="row mb-5 ">
           <div class="col-lg-12">
-            <h2 v-if="h2" class="display-2">Projekte</h2>
-            <h4 class="display-4 mt-5">Plugin</h4>
-            <h5 class="display-5 mt-4 ">Österreichischer Hardwarehersteller</h5>
-            <strong class="display-6 mt-3 ">Ziel</strong>
-            <p>Ticketplugin für Jira - Projektmanagementsoftware</p>
-            <strong class="display-6 mt-3">Technologien</strong>
+            <h2 v-if="h2" class="display-2">{{ $t('Projekte') }}</h2>
+            <h4 class="display-4 mt-5">{{ $t('plugin') }}</h4>
+            <h5 class="display-5 mt-4 ">
+              {{ $t('oesterreichischer-hardwarehersteller') }}
+            </h5>
+            <strong class="display-6 mt-3 ">{{ $t('ziel') }}</strong>
+            <p>{{ $t('ticketplugin-fuer-jira-projektmanagementsoftware') }}</p>
+            <strong class="display-6 mt-3">{{ $t('technologien') }}</strong>
             <p>Jira, Java</p>
             <br />
 
-            <h4 class="display-4 mt-5 ">Design</h4>
-            <h5 class="display-5 mt-4 ">Deutscher Tee Verkäufer in Amazon</h5>
-            <strong class="display-6 mt-3 ">Ziel</strong>
-            <p>Mockups für Tee Verpackungen</p>
-            <strong class="display-6 mt-3 ">Technologien</strong>
-            <p>Photoshop</p>
-            <h5 class="display-5 mt-4 ">Für schweizer Taxigesellschaften</h5>
-            <strong class="display-6 mt-3 ">Ziel</strong>
-            <p>Redesign eines Taximeter App für Android</p>
-            <strong class="display-6 mt-3 ">Technologien</strong>
-            <p>Photoshop (PSD), Android Studio Layout Editor (XMl)</p>
+            <h4 class="display-4 mt-5 ">{{ $t('design') }}</h4>
+            <h5 class="display-5 mt-4 ">
+              {{ $t('deutscher-tee-verkaeufer-in-amazon') }}
+            </h5>
+            <strong class="display-6 mt-3 ">{{ $t('ziel') }}</strong>
+            <p>{{ $t('mockups-fuer-tee-verpackungen') }}</p>
+            <strong class="display-6 mt-3 ">{{ $t('technologien') }}</strong>
+            <p>{{ $t('photoshop') }}</p>
+            <h5 class="display-5 mt-4 ">
+              {{ $t('fuer-schweizer-taxigesellschaften') }}
+            </h5>
+            <strong class="display-6 mt-3 ">{{ $t('ziel') }}</strong>
+            <p>{{ $t('redesign-eines-taximeter-app-fuer-android') }}</p>
+            <strong class="display-6 mt-3 ">{{ $t('technologien') }}</strong>
+            <p>{{ $t('photoshop-psd-android-studio-layout-editor-xml') }}</p>
             <br />
 
-            <h4 class="display-4 mt-5 ">Ghostwriting</h4>
-            <h5 class="display-5 mt-4 ">Deutsche E-Books</h5>
-            <strong class="display-6 mt-3 ">Ziel</strong>
-            <p>Ghostwriting / Zusammenfassungen und Formatierungen</p>
-            <strong class="display-6 mt-3 ">Technologien</strong>
-            <p>Microsoft Word</p>
+            <h4 class="display-4 mt-5 ">{{ $t('Ghostwriting') }}</h4>
+            <h5 class="display-5 mt-4 ">{{ $t('deutsche-e-books') }}</h5>
+            <strong class="display-6 mt-3 ">{{ $t('ziel') }}</strong>
+            <p>{{ $t('ghostwriting-zusammenfassungen-und-formatierungen') }}</p>
+            <strong class="display-6 mt-3 ">{{ $t('technologien') }}</strong>
+            <p>{{ $t('microsoft-word') }}</p>
             <br />
 
-            <h4 class="display-4 mt-5 ">Social Media Marketing - SMM</h4>
-            <h5 class="display-5 mt-4 ">Deutsche Reiseagentur</h5>
-            <strong class="display-6 mt-3 ">Ziel</strong>
+            <h4 class="display-4 mt-5 ">
+              {{ $t('social-media-marketing-smm') }}
+            </h4>
+            <h5 class="display-5 mt-4 ">{{ $t('deutsche-reiseagentur') }}</h5>
+            <strong class="display-6 mt-3 ">{{ $t('ziel') }}</strong>
             <p>Neue Kunden mit täglichen Posts inspirieren</p>
-            <strong class="display-6 mt-3 ">Technologien</strong>
+            <strong class="display-6 mt-3 ">{{ $t('technologien') }}</strong>
             <p>Facebook</p>
             <br />
 
@@ -46,33 +54,33 @@
               Zeitersparnis durch Automatisierungen
             </h4>
             <h5 class="display-5 mt-4 ">Prüffeld, VS</h5>
-            <strong class="display-5 mt-3 ">Ziel</strong>
+            <strong class="display-5 mt-3 ">{{ $t('ziel') }}</strong>
             <p>
               Wegfall von manuellen Eintragungen in Excel.<br />
               Interaktion mit Excel und Testprogramm.
             </p>
-            <strong class="display-5 mt-3 ">Technologien</strong>
+            <strong class="display-5 mt-3 ">{{ $t('technologien') }}</strong>
             <p>AutoIT und VBA</p>
 
             <h5 class="display-5 mt-5 ">
               Technische Abteilung, VS
             </h5>
-            <strong class="display-6 mt-3 ">Ziel</strong>
+            <strong class="display-6 mt-3 ">{{ $t('ziel') }}</strong>
             <p>
               Wegfall von manuellen Eintragungen in Excel, Outlook, Aegis,
               Clipboard, SAP.
             </p>
-            <strong class="display-6 mt-3 ">Technologien</strong>
+            <strong class="display-6 mt-3 ">{{ $t('technologien') }}</strong>
             <p>FSharp, AutoIT und VBA</p>
 
             <h5 class="display-5 mt-5 ">Strainovic IT</h5>
-            <strong class="display-6 mt-3 ">Ziel</strong>
+            <strong class="display-6 mt-3 ">{{ $t('ziel') }}</strong>
             <p>
               Durchforsten einer Webseiten und ausfüllen von Formularfeldern.
               Erstellt mit FSharp, Fsharp.Data ,Docker ,Canopy auf Google Cloud
               Plattform.
             </p>
-            <strong class="display-6 mt-3 ">Technologien</strong>
+            <strong class="display-6 mt-3 ">{{ $t('technologien') }}</strong>
             <p>FSharp, AutoIT und VBA</p>
             <br />
 
@@ -80,9 +88,9 @@
               Design von Printmedien
             </h4>
             <h5 class="display-5 mt-4 ">Strainovic IT</h5>
-            <strong class="display-6 mt-3 ">Ziel</strong>
+            <strong class="display-6 mt-3 ">{{ $t('ziel') }}</strong>
             <p>Visitenkarte, Flyer, Banner</p>
-            <strong class="display-6 mt-3 ">Technologien</strong>
+            <strong class="display-6 mt-3 ">{{ $t('technologien') }}</strong>
             <p>Photoshop, Illustrator, Inkscape, Canva.</p>
             <br />
 
@@ -101,21 +109,21 @@
             <h5 class="display-5 mt-4 ">
               rBranches für einen österreichischen Kunden
             </h5>
-            <strong class="display-6 mt-3 ">Ziel</strong>
+            <strong class="display-6 mt-3 ">{{ $t('ziel') }}</strong>
             <p>
               Programmierung eines D-A-CH Branchenverzeichnisses im Team
             </p>
-            <strong class="display-6 mt-3 ">Technologien</strong>
+            <strong class="display-6 mt-3 ">{{ $t('technologien') }}</strong>
             <p>R mit RStudio</p>
             <h5 class="display-5 mt-5 ">
               Plugin für einen deutschen Kunden
             </h5>
-            <strong class="display-6 mt-3 ">Ziel</strong>
+            <strong class="display-6 mt-3 ">{{ $t('ziel') }}</strong>
             <p>
               Unbenötigte Formularfelder in Shopware bei der Registrierung
               deaktivieren
             </p>
-            <strong class="display-6 mt-3 ">Technologien</strong>
+            <strong class="display-6 mt-3 ">{{ $t('technologien') }}</strong>
             <p>PHP</p>
             <br />
 
@@ -123,17 +131,17 @@
             <h5 class="display-5 mt-4 ">
               Schulung beim Kunden in Zürich
             </h5>
-            <strong class="display-6 mt-3 ">Ziel</strong>
+            <strong class="display-6 mt-3 ">{{ $t('ziel') }}</strong>
             <p>Basics der Webseitenerstellung</p>
-            <strong class="display-6 mt-3 ">Technologien</strong>
+            <strong class="display-6 mt-3 ">{{ $t('technologien') }}</strong>
             <p>WordPress, Divi</p>
 
             <h5 class="display-5 mt-5 ">
               Schulung eines Kunden aus St.Gallen
             </h5>
-            <strong class="display-6 mt-3 ">Ziel</strong>
+            <strong class="display-6 mt-3 ">{{ $t('ziel') }}</strong>
             <p>Basics der Programmierung</p>
-            <strong class="display-6 mt-3 ">Technologien</strong>
+            <strong class="display-6 mt-3 ">{{ $t('technologien') }}</strong>
             <p>F# (FSharp), Visual Studio</p>
             <br />
 
@@ -143,7 +151,7 @@
             <h5 class="display-5 mt-4 ">Strainovic IT</h5>
             <strong class="display-6 mt-3 ">Ziele</strong>
             <p>Besseres Projektmanagement</p>
-            <strong class="display-6 mt-3 ">Technologien</strong>
+            <strong class="display-6 mt-3 ">{{ $t('technologien') }}</strong>
             <p>Redmine, Odoo</p>
           </div>
         </div>
